cda7f48832044fcea13010f6937177e3f86b1ed5
quzelkurt <s.hussein@students.htl-leonding.ac.at>
2023-10-18 15:17:01 +0200

    changed some java

--- src/main/java/at/ac/htl/leonding/demo/App.java
+++ src/main/java/at/ac/htl/leonding/demo/App.java
@@ -8,4 +8,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
--- src/test/java/at/ac/htl/leonding/demo/AppTest.java
+++ src/test/java/at/ac/htl/leonding/demo/AppTest.java
@@ -1,5 +1,6 @@
 package at.ac.htl.leonding.demo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -14,5 +15,11 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    public void given_add_function_when_adding_2_and_3_then_result_5() {
+        var app = new App();
+        var result = app.add(2, 3);
+        assertEquals(5, result);
     }
 }
